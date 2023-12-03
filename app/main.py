d102e67ca7a5e6026b948357df802e7bc427bf9f
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-12-03 11:41:06 +0100

    updates
    can run stand alone and in docker, same entry point

--- app/main.py
+++ app/main.py
@@ -6,34 +6,41 @@ from model import yolo_model_pretrained
 
 app = FastAPI()
 
+# A dictionary to store image URLs and their corresponding image data
+image_urls = {}
+
+
 @app.get("/")
 def home():
     return "Hallo im runnin!"
 
+
 @app.post("/detect_objects")
-async def detect_objects(image:UploadFile = File(...)):
+async def detect_objects(image: UploadFile = File(...)):
     # convert uploadfile to Pil image 
     image_file = Image.open(BytesIO(await image.read()))
-  
+
     # get predidted image
-    predicted_img = yolo_model_pretrained.get_objects_detected(image_file)
+    predicted_classes, predicted_img = yolo_model_pretrained.get_objects_detected(image_file)
 
     # convert pil image to bytes
-    predicted_img_bytes= image_to_byte_array(predicted_img)
-    
-    return Response(content=predicted_img_bytes, media_type="image/jpg")
+    predicted_img_bytes = image_to_byte_array(predicted_img)
+
+    response = Response(content=predicted_img_bytes, media_type="image/jpg")
+    return response
+
 
 def image_to_byte_array(image: Image) -> bytes:
-  # BytesIO is a file-like buffer stored in memory
-  imgByteArr = BytesIO()
-  # image.save expects a file-like as a argument
-  image.save(imgByteArr, format=image.format)
-  # Turn the BytesIO object back into a bytes object
-  imgByteArr = imgByteArr.getvalue()
-  return imgByteArr
+    # BytesIO is a file-like buffer stored in memory
+    imgByteArr = BytesIO()
+    # image.save expects a file-like as a argument
+    image.save(imgByteArr, format=image.format)
+    # Turn the BytesIO object back into a bytes object
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
 
+    uvicorn.run("main:app", host="0.0.0.0", port=88, log_level="info")
--- app/model/yolo_model_pretrained.py
+++ app/model/yolo_model_pretrained.py
@@ -13,17 +13,40 @@ with open(f"{BASE_DIR}/object-detection-0.1.0.pkl", 'rb') as file:
 
 print("Ml rdy!")
 
+
 def get_objects_detected(image) -> Image:
     print(type(image))
     print(image)
-    
-    results = yolo_model(image)
 
-    im:Image = None
+    results = yolo_model.predict(image)
+
+    predicted_im = get_img_with_predicted(results)
+
+    predicted_classes = get_predicted_classes(results)
+
+    return predicted_classes, predicted_im
+
+
+def get_img_with_predicted(results):
+    im: Image = None
 
     for r in results:
         im_array = r.plot()  # plot a BGR numpy array of predictions
         im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
         im.format = "JPEG"
 
-    return im 
+    return im
+
+
+def get_predicted_classes(results):
+    predicted_classes = {}
+    # a dict af all classes
+    class_dict = results[0].names
+
+    for tensor in results[0].boxes.cls:
+        predicted_class = class_dict[int(tensor.item())]
+        if predicted_class not in predicted_classes:
+            predicted_classes[predicted_class] = 0
+        predicted_classes[predicted_class] += 1
+
+    return predicted_classes
